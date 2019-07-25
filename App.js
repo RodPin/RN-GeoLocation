582b679d6f92b386359f76af355f45fa3b744dea
Rodrigo Gomes <prodrigo@poli.ufrj.br>
2019-07-25 11:58:34 -0300

    Migrating to hooks

--- App.js
+++ App.js
@@ -6,100 +6,83 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Button, View, Text} from 'react-native';
-import Geolocation from 'react-native-geolocation-service';
-import Permissions from 'react-native-permissions';
-import firebaseConfig from './firebaseConfig'
-import firebase from 'firebase';
+import React, { useState, useEffect } from "react";
+import { Button, View, Text } from "react-native";
+import Geolocation from "react-native-geolocation-service";
+import Permissions from "react-native-permissions";
+import firebase from "firebase";
+import firebaseConfig from "./firebaseConfig";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      lat: null,
-      long: null,error:null,
-      count:0
-    };
-  }
+var contador = 0;
 
-  pushToFirebase(json){
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [latLong, setLatLong] = useState({});
+  const [error, setError] = useState(null);
+
+  function pushToFirebase(json) {
     firebase
-    .database()
-    .ref()
-    .child("location/")
-    .set(json)
+      .database()
+      .ref()
+      .child("location/")
+      .set(json);
   }
 
-  componentWillMount(){
-    firebase.initializeApp(firebaseConfig);
-  }
+  useEffect(() => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+    Permissions.request("location").then(response => {
+      console.log(response);
+    });
+  }, []);
 
-  componentDidMount() {
-    Permissions.request('location').then(response => {
-      // Returns once the user has chosen to 'allow' or to 'not allow' access
-      // Response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
-      this.setState({hasLocationPermission: response});
-    })
-
-    // Instead of navigator.geolocation, just use Geolocation.
-  }
-
-  render() {
-    const {lat, long,error,count} = this.state;
-    return (
-      <View style={{flex: 1, backgroundColor: 'grey'}}>
-        <Button
+  return (
+    <View style={{ flex: 1, backgroundColor: "grey" }}>
+      {/* <Button
           title="get my loc"
           onPress={() => {
             Geolocation.getCurrentPosition(
+
               position => {
                 const coords = position.coords;
-                this.setState({
-                  lat: coords.latitude,
-                  long: coords.longitude
-                });
+                setLatLong({lat:coords.latitude,long:coords.longitude})
               },
               error => {
                 // See error code charts below.
-                console.log(error.code, error.message);
+                setError(error.message)
               },
               {enableHighAccuracy: true, timeout: 15000,}
             );
           }}
-        />
-         <Button
-          title="follow my loc"
-          onPress={() => {
-            Geolocation.watchPosition(
-              position => {
-                const coords = position.coords;
-                this.setState({
-                  lat: coords.latitude,
-                  long: coords.longitude,
-                  count:this.state.count+1
-                });
-                this.pushToFirebase({lat: coords.latitude,long: coords.longitude})
-              },
-              error => {
-                // See error code charts below.
-                this.setState({error:error.message
-                })
-              },
-              {enableHighAccuracy: true, interval: 10000,distanceFilter:0}
-            );
-          }}
-        />
-         
-        <Text style={{fontSize: 24}}>lat: {lat}</Text>
-        <Text style={{fontSize: 24}}>lat: {long}</Text>
-        <Text style={{fontSize: 24}}>count: {count}</Text>
-        <Text style={{fontSize:24,color:'red'}}>{error}</Text>
+        /> */}
+      <Button
+        title="My loc"
+        onPress={() => {
+          Geolocation.watchPosition(
+            position => {
+              contador++;
+              const coords = position.coords;
+              setLatLong({ lat: coords.latitude, long: coords.longitude });
+              setCount(contador);
+              pushToFirebase({ lat: coords.latitude, long: coords.longitude });
+            },
+            error => {
+              // See error code charts below.
 
-        
-      </View>
-    );
-  }
-}
+              setError(error.message);
+            },
+            { enableHighAccuracy: true, interval: 10000, distanceFilter: 0 }
+          );
+        }}
+      />
+
+      <Text style={{ fontSize: 24 }}>lat: {latLong.lat}</Text>
+      <Text style={{ fontSize: 24 }}>lat: {latLong.long}</Text>
+      <Text style={{ fontSize: 24 }}>count: {count}</Text>
+      <Text style={{ fontSize: 24, color: "red" }}>{error}</Text>
+    </View>
+  );
+};
 
 export default App;
