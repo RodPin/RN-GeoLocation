5e72f086885d633a85da61e5414779d29bcda6f6
RODRIGO <prodrigo@poli.ufrj.br>
2019-07-26 18:49:10 -0300

    saving at firebase

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import {View, ScrollView, Text, TextInput, Button} from 'react-native';
+import {
+  View,
+  ScrollView,
+  Text,
+  TextInput,
+  Button,
+  ActivityIndicator
+} from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 import Permissions from 'react-native-permissions';
 import firebase from 'firebase';
@@ -8,86 +15,107 @@ import BackgroundTimer from 'react-native-background-timer';
 import {ListItem, List, Left, Body, Right} from 'native-base';
 var contador = 0;
 
-const obj = {
-  rodrigo: {
-    lat: 23.2342,
-    long: 34.9421
-  },
-  renato: {
-    lat: 23.2342,
-    long: 34.9421
-  },
-  luis: {
-    lat: 23.2342,
-    long: 34.9421
-  },
-  jenifer: {
-    lat: 23.2342,
-    long: 34.9421
-  }
-};
-
 const App = () => {
   const [name, setName] = useState(null);
-  const [confirmedName, setConfirmedName] = useState('sads');
+  const [confirmedName, setConfirmedName] = useState('dddd');
   const [count, setCount] = useState(0);
   const [latLong, setLatLong] = useState({});
   const [error, setError] = useState(null);
+  const [obj, setObj] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   function pushToFirebase(json) {
+    const now = new Date();
+    const hour = now.getHours() + ':' + now.getMinutes();
+    const date = now.getDay() + '/' + now.getMonth() + '/' + now.getFullYear();
+    json = {...json, hour, date};
     firebase
       .database()
       .ref()
-      .child('location/' + confirmedName)
+      .child('locations/' + confirmedName)
       .set(json)
-      .then(x => alert(x))
+      .then(x => {})
       .catch(err => {
         if (err.message == 'PERMISSION_DENIED: Permission denied') {
-          setError('Permission denied');
+          setError('Saving at database denied');
         }
+      });
+  }
+  function loadFromFirebase() {
+    firebase
+      .database()
+      .ref('locations')
+      .on('value', snap => {
+        var list = [];
+        snap.forEach(data => {
+          list.push({
+            key: data.key,
+            data: data.val()
+          });
+        });
+        setObj(list);
+        setLoading(false);
       });
   }
   function renderCards() {
     var aux = [];
-    const names = Object.keys(obj);
-    names.map(name => {
-      aux.push(<Card name={name} lat={obj[name].lat} long={obj[name].long} />);
-    });
-    return aux;
+    if (loading) {
+      return <ActivityIndicator color="white" size="large" />;
+    }
+    if (obj) {
+      obj.map(person => {
+        aux.push(
+          <Card
+            name={person.key}
+            lat={person.data.lat}
+            long={person.data.long}
+            date={person.data.date}
+            hour={person.data.hour}
+          />
+        );
+      });
+      return aux;
+    }
   }
-
+  function getLocation() {
+    Geolocation.getCurrentPosition(
+      position => {
+        contador++;
+        const coords = position.coords;
+        const json = {lat: coords.latitude, long: coords.longitude};
+        setLatLong(json);
+        setCount(contador);
+        pushToFirebase(json);
+      },
+      error => {
+        // See error code charts below.
+        setError(error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000}
+    );
+  }
   useEffect(() => {
     if (!firebase.apps.length) {
       firebase.initializeApp(firebaseConfig);
     }
+    setLoading(true);
+
+    setTimeout(() => loadFromFirebase(), 1000);
     Permissions.request('location').then(response => {
       console.log(response);
     });
-
+    getLocation();
     BackgroundTimer.setInterval(() => {
       // this will be executed every 200 ms
       // even when app is the the background
       if (confirmedName) {
-        Geolocation.getCurrentPosition(
-          position => {
-            contador++;
-            const coords = position.coords;
-            setLatLong({lat: coords.latitude, long: coords.longitude});
-            setCount(contador);
-            pushToFirebase({lat: coords.latitude, long: coords.longitude});
-          },
-          error => {
-            // See error code charts below.
-            setError(error.message);
-          },
-          {enableHighAccuracy: true, timeout: 15000}
-        );
+        getLocation();
       }
     }, 30000);
   }, []);
   if (confirmedName) {
     return (
-      <View
+      <ScrollView
         style={{
           flex: 1,
           padding: 40,
@@ -100,11 +128,11 @@ const App = () => {
         <Text style={{fontSize: 24}}>i got your location {count} times</Text>
         <Text style={{fontSize: 24, color: 'red'}}>{error}</Text>
         <List>{renderCards()}</List>
-      </View>
+      </ScrollView>
     );
   }
   return (
-    <ScrollView
+    <View
       style={{
         flex: 1,
         padding: 30,
@@ -123,23 +151,24 @@ const App = () => {
         color="green"
         onPress={() => setConfirmedName(name)}
       />
-    </ScrollView>
+    </View>
   );
 };
 
 export default App;
 
-const Card = ({name, lat, long}) => (
+const Card = ({name, lat, long, date, hour}) => (
   <ListItem>
-    <Left>
+    <Left style={{flex: 0.3}}>
       <Text style={{fontSize: 24, color: 'white'}}>{name}</Text>
     </Left>
-    <Body>
-      <Text style={{fontSize: 18, color: 'white'}}>lat {lat}</Text>
-      <Text style={{fontSize: 18, color: 'white'}}>long {long}</Text>
+    <Body style={{flex: 0.4}}>
+      <Text style={{fontSize: 18, color: 'white'}}>{lat}</Text>
+      <Text style={{fontSize: 18, color: 'white'}}>{long}</Text>
     </Body>
-    <Right>
-      <Text style={{fontSize: 16, color: 'white'}}>HI</Text>
+    <Right style={{flex: 0.3, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontSize: 11, color: 'white'}}>{date}</Text>
+      <Text style={{fontSize: 14, color: 'white'}}>{hour}</Text>
     </Right>
   </ListItem>
 );
