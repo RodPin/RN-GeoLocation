0f24e41bdc9067774648e1323dcdcfb20532d33b
Rodrigo Pinto Coelho Gomes <41244788+RodPin@users.noreply.github.com>
2019-10-08 15:53:13 -0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "config",
+    authDomain: "config',
+    databaseURL: "config",
+    projectId: "config",
+    storageBucket: "config",
+    messagingSenderId: "config",
+    appId: "config"
+  };
